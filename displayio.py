a2fca7d64169ae8b38d86bd9a87a41d07fc79d78
Melissa LeBlanc-Williams <melissa@adafruit.com>
2020-05-22 09:44:14 -0700

    Added proper headers, fixed pylint in actions

--- displayio.py
+++ displayio.py
@@ -1,5 +1,38 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2020 Melissa LeBlanc-Williams for Adafruit Industries
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 """
 `displayio`
+================================================================================
+
+displayio for Blinka
+
+**Software and Dependencies:**
+
+* Adafruit Blinka:
+  https://github.com/adafruit/Adafruit_Blinka/releases
+
+* Author(s): Melissa LeBlanc-Williams
+
 """
 
 import os
@@ -10,21 +43,6 @@ import threading
 import numpy
 from collections import namedtuple
 from PIL import Image, ImageDraw, ImagePalette
-
-"""
-import asyncio
-import signal
-import subprocess
-"""
-
-# Don't import pillow if we're running in the CI. We could mock it out but that
-# would require mocking in all reverse dependencies.
-if "GITHUB_ACTION" not in os.environ and "READTHEDOCS" not in os.environ:
-    # This will only work on Linux
-    pass
-else:
-    # this would be for Github Actions
-    utils = None  # pylint: disable=invalid-name
 
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_Blinka_displayio.git"
