1566aef5c5979ee018da8fec5786f9d824efa57a
Jan Volckaert <janvolck@gmail.com>
2023-12-31 15:12:05 +0100

    fix: use original self._reset.switch_to_output(value=True) from main branch

--- displayio/_fourwire.py
+++ displayio/_fourwire.py
@@ -74,7 +74,7 @@ class FourWire:
 
         if reset is not None:
             self._reset = digitalio.DigitalInOut(reset)
-            self._reset.switch_to_output(value=False)
+            self._reset.switch_to_output(value=True)
         else:
             self._reset = None
         self._spi = spi_bus
